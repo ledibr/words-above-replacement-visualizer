640d3d00cd89f5c09584b674e63c030ae5af1b0f
ledibr <ldial@brandeis.edu>
2026-05-05 11:47:20 -0400

    Fixed metadata annotation process and updated projector config
    Signed-off-by: ledibr <ldial@brandeis.edu>

--- annotate.py
+++ annotate.py
@@ -5,8 +5,8 @@ def annotate_metadata(size: str = '10k'):
     with open('metadata/player_list.txt', 'r', encoding='utf-8') as f:
         players = [line.strip() for line in f]
 
-    metadata_path = f'oss_data/war_word2vec_{size}_metadata.tsv'
-    metadata_df = pd.read_csv(metadata_path, encoding='utf-8', sep='\t', names=['word'])
+    metadata_path = f'oss_data/war_word2vec_{size}_metadata'
+    metadata_df = pd.read_csv(f'{metadata_path}.tsv', encoding='utf-8', sep='\t', names=['word'])
     wordcount_df = pd.read_csv(f'metadata/word_counts_{size}.csv', encoding='utf-8')
     batter_df = pd.read_csv(f'metadata/batter_stats_calc.csv', encoding='utf-8', header=0, names=['word', 'position', 'team'], usecols=[0, 1, 3])
     pitcher_df = pd.read_csv(f'metadata/pitcher_stats_calc.csv', encoding='utf-8', header=0, names=['word', 'position', 'team'], usecols=[0, 1, 3])
@@ -16,7 +16,7 @@ def annotate_metadata(size: str = '10k'):
     metadata_df = pd.merge(metadata_df, wordcount_df, how='left', on='word')
     metadata_df = pd.merge(metadata_df, player_df, how='left', on='word')
 
-    metadata_df.to_csv(metadata_path, sep='\t', index=False, mode='w', encoding='utf-8')
+    metadata_df.to_csv(f'{metadata_path}_annotated.tsv', sep='\t', index=False, mode='w', encoding='utf-8')
 
 
 if __name__ == '__main__':
